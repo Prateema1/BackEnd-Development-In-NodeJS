8d58bb20ac869e7ae04bff33e2a6bf97049f9392
Pratima <amitrana@MacUser.local>
2023-07-19 17:26:42 +0530

    Set up middleware for Express

--- Express/index.js
+++ Express/index.js
@@ -5,8 +5,20 @@ const PORT = 3001;
 // express function return app server object
 const app = express();
 
-app.get('/home', (request, response) => {
+const mylogger = (req, res, next) => {
+    console.log("Logging from middleware1");
+    next();
+}
+
+const exlogger = (req, res, next) => {
+    console.log("Logging from middleware2");
+    next();
+}
+
+app.get('/home', mylogger, exlogger, (request, response) => {
     // response.send("Hi there, welcome to get");
+
+    console.log("Last Middleware");  // Last middleware actually sends the request
     response.json({
         message: "OK get"
     })
